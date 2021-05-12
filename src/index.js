6f34d30cc1c09ae6e43d17b083e671c56a7eca3b
qiaoxx <qiaoxx@300624.cn>
2021-05-12 23:34:10 +0800

    Feat: 补充笔记

--- src/index.js
+++ src/index.js
@@ -2,12 +2,3 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(<h1>hello</h1>, document.getElementById('root'))
-
-function deepFreeze(obj) {
-    Object.freeze(obj);
-    for(let key in obj) {
-        if(typeof obj[key] == 'object') {
-            deepFreeze(obj[key])
-        }
-    }
-}
