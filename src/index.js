b67d2cd6671a63dab6e287fd319aacc1fde96c55
leylatop <775177772@qq.com>
2021-06-15 14:59:30 +0800

    Feat: 新增createElement方法

--- src/index.js
+++ src/index.js
@@ -1,4 +1,41 @@
-import React from 'react';
+import React from './react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>test</h1>, document.getElementById('root'))
+// let element = (
+//     <div className="title" style={{color: 'red'}}>
+//         <span>hello </span> world
+//     </div>
+// )
+// 经过babel转化之后，会转化成下面的代码
+let element = React.createElement('div', {
+    className: 'title',
+    style: {
+        color: 'red'
+    }
+}, React.createElement('span', null, 'hello'), 'world')
+console.log(JSON.stringify(element, null, 2));
+// {
+//     "type": "div",
+//     "key": null,
+//     "ref": null,
+//     "props": {
+//       "className": "title",
+//       "style": {
+//         "color": "red"
+//       },
+//       "children": [
+//         {
+//           "type": "span",
+//           "key": null,
+//           "ref": null,
+//           "props": {
+//             "children": "hello "
+//           },
+//           "_owner": null,
+//           "_store": {}
+//         },
+//         " world"
+//       ]
+//     },
+// }
+ReactDOM.render(element, document.getElementById('root'))
---
+++ src/react.js
@@ -0,0 +1,56 @@
+import { wrapToVdom } from "./utils";
+
+// 创建虚拟节点
+// 第一个参数，dom元素的类型
+// 第二个参数，props 包含属性、样式、事件等
+// 第三个参数，子元素
+// 如果有3+个参数，后面的参数都是子元素
+function createElement(type, config, children) {
+    // 展开config
+    let props = {...config};
+    // 如果参数大于3个，则后面的都是children， 从下标为2的参数开始往后截取
+    if(arguments.length > 3) {
+        children = Array.prototype.slice.call(arguments, 2).map(wrapToVdom);
+    }
+    // 将children放在props中 children放在props中
+    // children的值可能是
+    // 1. 字符串
+    // 2. 数字
+    // 3. undefined
+    // 4. null
+    // 5. 数组
+    props.children = wrapToVdom(children);      //wrapToVdom对字符串和数字进行加工处理
+
+    // {
+    //     "type": "div",
+    //     "key": null,
+    //     "ref": null,
+    //     "props": {
+    //     "className": "title",
+    //     "style": {
+    //         "color": "red"
+    //     },
+    //     "children": [
+    //         {
+    //         "type": "span",
+    //         "key": null,
+    //         "ref": null,
+    //         "props": {
+    //             "children": "hello "
+    //         }
+    //         },
+    //         " world"
+    //     ]
+    //     },
+    // }
+    return {
+        type,
+        props,
+    }
+}
+
+const React = {
+    createElement
+}
+
+export default React;
