dc36997b3f3e9feda65d13efc7f0d027ecdccba6
leyla_qiao <775177772@qq.com>
2021-06-26 00:41:12 +0800

    Feat: 类组件的渲染

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from './react';
+import ReactDOM from './react-dom';
 
 // let element = (
 //     <div className="title" style={{color: 'red'}}>
@@ -17,19 +17,23 @@ import ReactDOM from 'react-dom';
 function ComponentFunction(props) {
 	return React.createElement('h1', null, React.createElement('span',null, 'hello,'), props.name)
 }
+
+console.log(React.Component);
 class ClassComponent extends React.Component {
 	render() {
 		return <div>类组件</div>
 	}
 }
+console.log(React.Component);
+console.log(ClassComponent);
 // 类组件
 let element1 = <ComponentFunction name="qiao"/>;
 let element2 = <ClassComponent name="qiao"/>;
 // let element2 = React.createElement(ClassComponent, {name: 'qiao'})
 
 
-console.log(element1);
-console.log(element2);
+// console.log(element1);
+// console.log(element2);
 // {
 //     "type": "div",
 //     "key": null,
--- src/react-dom.js
+++ src/react-dom.js
@@ -1,4 +1,13 @@
+/*
+ * @Author: your name
+ * @Date: 2021-06-25 21:50:25
+ * @LastEditTime: 2021-06-25 23:06:13
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \react-source-self\src\react-dom.js
+ */
 import {REACT_TEXT} from './constants'
+
 // 把虚拟dom转化成真实dom并且插入到容器中
 function render(vdom, container) {
 	// 1. 根据虚拟dom转化成真实dom
@@ -14,10 +23,16 @@ function createDOM(vdom) {
 	// 如果节点是文本节点，就用createTextNode方法创建节点
 	if(type === REACT_TEXT) {
 		dom = document.createTextNode(props.content);
-	} else if(typeof type === 'function') {	// 函数组件
+	} else if(typeof type === 'function') { // 函数组件或类组件
 		// 这里直接return真实dom，是因为函数组件的props是传参使用的，没有必要挂载到真实dom上面；也没有儿子
 		// 所以就不需要走下面updateProps和reconcileChildren的流程
-		return mountFunctionComponent(vdom);
+		console.log(type);
+		// 函数组件
+		if(type.isReactComponent) {		// 说明是个类组件
+			return mountClassComponent(vdom);
+		} else {	// 说明是函数组件
+			return mountFunctionComponent(vdom);
+		}
 	} else {
 		dom = document.createElement(type)
 	};
@@ -41,10 +56,19 @@ function createDOM(vdom) {
 	return dom;
 }
 
+// 解析类组件
+function mountClassComponent(vdom) {
+	let {type, props} = vdom;	// 组件是类组件，所以需要实例化
+	let classInstance = new type(props);
+	// 执行render函数，得到组件内部的reactvdom
+	let renderVdom = classInstance.render();
+	return createDOM(renderVdom);
+}
+
 // 解析函数组件
 function mountFunctionComponent(vdom) {
 	let {type, props} = vdom;
-	let renderVdom = type(props);	// 返回组件内部的reactvdom
+	let renderVdom = new type(props);	// 返回组件内部的reactvdom
 	return createDOM(renderVdom);	// 将vdom转化成真实dom返回
 
 }
